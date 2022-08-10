ebca8d2d134f837eb44c68057ab30534ecfb2bd9
Ulukbek <orozgozhoev.u@gmail.com>
2022-08-10 10:57:08 +0600

    init project

--- src/App.js
+++ src/App.js
@@ -11,11 +11,11 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://google.com"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Open Google
         </a>
       </header>
     </div>
